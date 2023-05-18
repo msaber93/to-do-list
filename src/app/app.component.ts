2f868e49612f0253245e7169a6ce2735662d36e9
Mahmoud Saber <engabosaber2014@gmail.com>
2023-05-18 17:47:49 +0200

    make to do list code html css ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'to-do-project';
+  cricleplus = faCirclePlus;
+  todos: any[] = [];
+
+  addTodo(task: any) {
+    this.todos.push({ id: this.uid(), name: task, isDone: false });
+    console.log(this.todos);
+  }
+
+  uid() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  }
+
+  removeItem(id: number) {
+    this.todos = this.todos.filter((outPut) => outPut.id !== id);
+  }
+
+  updateValue(event: any, todoId: any) {
+    this.todos = this.todos.map(todo => {
+      if(todo.id === todoId) {
+        console.log(todoId)
+        todo.isDone = event.target.checked;
+      }
+      return todo
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
